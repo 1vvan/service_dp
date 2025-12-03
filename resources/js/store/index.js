259b9c8f057e18a23cc38369f1a068620eebcbab
krasnikov-ia <krasnikov_ia@groupbwt.com>
2025-12-03 09:22:57 +0200

    Initial commit: Laravel + Vue.js + Vue Router + Vuex

---
+++ resources/js/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+    state: {
+        // Здесь будет состояние приложения
+    },
+    mutations: {
+        // Здесь будут мутации
+    },
+    actions: {
+        // Здесь будут действия
+    },
+    modules: {
+        // Здесь будут модули
+    }
+});
+
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mx-auto px-4 py-8">
+        <h1 class="text-3xl font-bold mb-4">Добро пожаловать!</h1>
+        <p class="text-lg">Laravel + Vue.js + Vue Router + Vuex настроены и готовы к работе.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
